2210c8e2e996b04c324f2c5a260bdb08912beeb5
thaibinh156 <binh.thaidiep@infodation.vn>
2024-11-06 09:24:01 +0700

    Edit User class

--- src/main/java/com/infodation/userservice/models/User.java
+++ src/main/java/com/infodation/userservice/models/User.java
@@ -26,7 +26,7 @@ public class User {
     private String userId;
 
     @Column(name = "first_name")
-    @JsonAlias("first_name")
+    @JsonAlias("firstName")
     private String firstName;
 
     @Column(name = "last_name")
