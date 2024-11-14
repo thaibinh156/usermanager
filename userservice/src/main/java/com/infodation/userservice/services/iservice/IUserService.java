eb78559e236457aa4755e6ba13a6c90d75dad291
huyluong22 <huyluong22@gmail.com>
2024-11-14 15:14:40 +0700

    apply paging for get all users

--- userservice/src/main/java/com/infodation/userservice/services/iservice/IUserService.java
+++ userservice/src/main/java/com/infodation/userservice/services/iservice/IUserService.java
@@ -3,11 +3,13 @@ package com.infodation.userservice.services.iservice;
 import com.infodation.userservice.models.User;
 import com.infodation.userservice.models.dto.user.CreateUserDTO;
 import com.infodation.userservice.models.dto.user.UpdateUserDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface IUserService {
-    List<User> getAll();
+    Page<User> getAll(Pageable pageable);
     User getByUserId(String userId);
     User save(CreateUserDTO user);
     User update(String userId,UpdateUserDTO user);
